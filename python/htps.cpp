44856227521604f57bdc25bd7ddc5cba4e09958a
Simon Sorg <simon.sorg@student.hpi.de>
2025-03-29 21:29:27 +0000

    Comment out to test

--- python/htps.cpp
+++ python/htps.cpp
@@ -1016,8 +1016,8 @@ PyObject* Theorem_NewFromShared(const std::shared_ptr<htps::theorem>& thm_ptr) {
     c_obj->set_context(thm->context);
     c_obj->hypotheses = thm->hypotheses;
     c_obj->past_tactics = thm->past_tactics;
-    c_obj->py_dict = thm->py_dict;
-    Py_INCREF(c_obj->py_dict);
+//    c_obj->py_dict = thm->py_dict;
+//    Py_INCREF(c_obj->py_dict);
     return obj;
 }
 
